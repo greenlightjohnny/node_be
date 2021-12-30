e322c2023977d583b52328cd37a4a2aa6fdc9075
Fred <castelloiv@gmail.com>
2021-12-30 02:27:16 -0500

    Updating

---
+++ src/controller/session.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { createSession } from "../service/session.service";
+import { validatePassword } from "../service/user.service";
+
+
+export async function createUserSessionHandler(req: Request, res: Response) {
+  //check password
+  const user = await validatePassword(req.body, req.body);
+  if (!user) {
+    return res.status(401).send("Invalid user, not found");
+  }
+
+  //create session
+  const session = await createSession(user._id, req.get("user-agent") || "");
+
+  //create access token
+  const accessToken = signJwt({
+    ...user, session: session._id,
+    {expiresIn: process.env.accessTokenTTL}
+  })
+  //create refresh token
+  //Return access and refresh tokens
+}
--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,6 +2,7 @@ import { Response, Request } from "express";
 import { createUser } from "../service/user.service";
 import logger from "../utils/logger";
 import { CreateUserInput } from "../schema/user.schema";
+import { omit } from "lodash";
 
 export async function createUserHandler(
   req: Request<{}, {}, CreateUserInput["body"]>,
@@ -9,7 +10,8 @@ export async function createUserHandler(
 ) {
   try {
     const user = await createUser(req.body);
-    return user;
+    return res.send(omit(user.toJSON(), "password"));
+    //return res.send(user.n);
   } catch (e: any) {
     logger.error(e);
     return res.status(409).send(e.message);
