467584c1beea9256d79c7b7527eecdf63b118e6f
Fred <castelloiv@gmail.com>
2021-12-29 19:49:23 -0500

    Logging added, db connection troubleshooting

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,6 +1,24 @@
 import mongoose from "mongoose";
 import config from "config";
 
-const connect = () => {};
+const connect = async () => {
+  const dbUri = config.get<string>("dbUri");
+  console.log(dbUri);
+
+  try {
+    await mongoose.connect(dbUri);
+    console.log("DB connected yay!");
+  } catch (error) {
+    console.error("Could not connect to DB");
+    process.exit(1);
+  }
+  // .then .catch version
+  // return mongoose.connect(dbUri).then(() => {
+  //   console.log("Connected to DB")
+  // }).catch((error) => {
+  //   console.error("Could not connect to DB")
+  //   process.exit(1)
+  // })
+};
 
 export default connect;
