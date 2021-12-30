d869280b15cc74220e42025f72328856c2de7cd0
Fred <castelloiv@gmail.com>
2021-12-29 22:07:58 -0500

    Changing types and version for mongoose to avoid error with schema

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -2,6 +2,14 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import config from "config";
 
+export interface UserDocument extends mongoose.Document {
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updateAt: Date;
+}
+
 const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
@@ -12,6 +20,13 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next: mongoose.HookNextFunction) {
+  let user = this as UserDocument;
+
+  if (!user.isModified) {
+  }
+});
 
 const UserModel = mongoose.model("User", userSchema);
 
